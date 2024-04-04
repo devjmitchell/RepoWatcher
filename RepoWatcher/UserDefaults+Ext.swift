483c6428b81facc937d5d978b8f570d97260ac11
Jason Mitchell <devjmitchell@gmail.com>
2024-04-04 10:09:03 -0400

    Create app UI to prep for widget intents

---
+++ RepoWatcher/UserDefaults+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  UserDefaults+Ext.swift
+//  RepoWatcher
+//
+//  Created by Jason Mitchell on 4/4/24.
+//
+
+import Foundation
+
+extension UserDefaults {
+    static var shared: UserDefaults {
+        UserDefaults(suiteName: "group.com.semperphoenix.RepoWatcher")!
+    }
+    
+    static let repoKey = "repos"
+}
